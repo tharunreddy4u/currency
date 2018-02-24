102f01d43da262eaaa759f9883855bf1be9eb2f0
tharunreddy4u <tharun.reddy4u@gmail.com>
2018-02-23 19:25:42 -0500

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule} from '@angular/http';  
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { UserComponent }  from './components/user/user.component';
 
 @NgModule({
-  imports:      [ BrowserModule, HttpModule ],
+  imports:      [ BrowserModule, HttpModule, FormsModule ],
   declarations: [ AppComponent,UserComponent ],
   providers: [],
   bootstrap:    [ AppComponent ]
